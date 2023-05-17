85009620241e3c244284c1bdc22dc224a8cfbed2
entest <hai@entst.io>
2023-05-17 06:55:51 +0000

    day-2

--- day-1/run.sh
+++ day-1/run.sh
@@ -1,14 +1,7 @@
 aws cloudformation create-stack \
  --stack-name cfn-lambda-demo \
- --template-body file://lambda_template.yaml \
+ --template-body file://lambda.yaml \
  --capabilities CAPABILITY_NAMED_IAM
-
-#  aws cloudformation create-stack \
-#  --stack-name cfn-lambda-demo \
-#  --template-body file://lambda_template.yaml \
-#  --capabilities CAPABILITY_NAMED_IAM \
-#  --role-arn arn:aws:iam::610770234379:role/cdk-admin-role-CdkAdminRole-1IBORD9375GQF
-
 
 # aws cloudformation delete-stack \
 #  --stack-name cfn-lambda-demo
--- day-2/run.sh
+++ day-2/run.sh
@@ -4,12 +4,5 @@ aws cloudformation create-stack \
  --parameters ParameterKey=InstanceType,ParameterValue=t2.medium \
  --capabilities CAPABILITY_NAMED_IAM
 
-# aws cloudformation update-stack \
-#   --stack-name cfn-vpc-demo \
-#   --template-body file://vpc.yaml \
-#   --parameters ParameterKey=InstanceType,ParameterValue=t2.medium \
-#   --capabilities CAPABILITY_NAMED_IAM
-
-
 #  aws cloudformation delete-stack \
 #  --stack-name cfn-vpc-demo 
