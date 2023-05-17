3dfa3153956b79ef1f8b4746ac959375be9907a8
entest <hai@entst.io>
2023-05-17 06:54:15 +0000

    day-2

---
+++ day-2/run.sh
@@ -0,0 +1,15 @@
+aws cloudformation create-stack \
+ --stack-name cfn-vpc-demo \
+ --template-body file://vpc.yaml \
+ --parameters ParameterKey=InstanceType,ParameterValue=t2.medium \
+ --capabilities CAPABILITY_NAMED_IAM
+
+# aws cloudformation update-stack \
+#   --stack-name cfn-vpc-demo \
+#   --template-body file://vpc.yaml \
+#   --parameters ParameterKey=InstanceType,ParameterValue=t2.medium \
+#   --capabilities CAPABILITY_NAMED_IAM
+
+
+#  aws cloudformation delete-stack \
+#  --stack-name cfn-vpc-demo 
